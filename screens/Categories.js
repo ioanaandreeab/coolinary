3eba34422597107f187bba63ff01359d5a2bb29e
Ioana Berdei <andreea.ioanaa98@gmail.com>
2021-04-19 17:26:50 +0300

    add bottom tabs navigation

---
+++ screens/Categories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, Layout } from '@ui-kitten/components';
+
+const Categories = () => (
+  <Layout style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <Text>Categories Screen</Text>
+  </Layout>
+)
+
+export default Categories;
---
+++ screens/SavedRecipes.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, Layout } from '@ui-kitten/components';
+
+const SavedRecipes = () => (
+  <Layout style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <Text>SavedRecipes Screen</Text>
+  </Layout>
+)
+
+export default SavedRecipes;
